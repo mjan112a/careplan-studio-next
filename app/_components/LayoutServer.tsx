6881f2c15c1af277fe0cc8ed056dbae9195fbb7a
Michael Marshall <marshall@shadowarc.com>
2025-04-28 09:43:09 -0400

    FIX:  Cookies await

--- app/_components/LayoutServer.tsx
+++ app/_components/LayoutServer.tsx
@@ -10,14 +10,14 @@ interface LayoutServerProps {
 }
 
 export default async function LayoutServer({ children }: LayoutServerProps) {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
+        async get(name: string) {
           const cookie = cookieStore.get(name);
           return cookie?.value;
         },
